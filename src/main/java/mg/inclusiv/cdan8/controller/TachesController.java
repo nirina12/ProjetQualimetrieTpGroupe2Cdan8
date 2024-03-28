387901c6a1e3e129b58e1a92313e316788906310
gitArnel456 <arnelmihaja@gmail.com>
2024-03-28 12:09:56 +0300

    commit stage

--- src/main/java/mg/inclusiv/cdan8/controller/TachesController.java
+++ src/main/java/mg/inclusiv/cdan8/controller/TachesController.java
@@ -1,18 +1,17 @@
+
 package mg.inclusiv.cdan8.controller;
 
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import mg.inclusiv.cdan8.Services.TacheService;
-import mg.inclusiv.cdan8.entity.Tache;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import mg.inclusiv.cdan8.Services.TacheService;
+import mg.inclusiv.cdan8.entity.Tache;
 
 @RestController
 @RequestMapping("/tache")
@@ -22,7 +21,7 @@ public class TachesController {
     
     @GetMapping("list")
     public List<Tache> listTache() {
-       return tacheService.tacheList();
+        return tacheService.tacheList();
     }
     @PostMapping("Save")
     public void saveTache(Tache tache){
