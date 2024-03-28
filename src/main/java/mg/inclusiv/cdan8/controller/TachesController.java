49148b318d0c7cc24f4cde868fd38eb9a428f94a
[Haritiana] <[rhartiana.fanomezana@mgail.com]>
2024-03-28 15:45:08 +0300

    affichage tache

--- src/main/java/mg/inclusiv/cdan8/controller/TachesController.java
+++ src/main/java/mg/inclusiv/cdan8/controller/TachesController.java
@@ -1,27 +1,42 @@
 package mg.inclusiv.cdan8.controller;
 
-import mg.inclusiv.cdan8.Services.TacheService;
-import mg.inclusiv.cdan8.entity.Tache;
+
+import java.util.List;
+
+import mg.inclusiv.cdan8.repository.TacheRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
+import mg.inclusiv.cdan8.Services.TacheService;
+import mg.inclusiv.cdan8.entity.Tache;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/dashboard")
-
 public class TachesController {
-    
-    TacheService tacheService;
-    @GetMapping("")
-    public String viewhome(Model model) {
-        //List<Tache> tacheList= tacheService.tacheList();
-        //model.addAttribute("tacheList", tacheList);
-        return "dasboard";
+    @Autowired
+    TacheRepository tacheRepository;
+
+    @GetMapping("/list")
+    public String listTache(Model model) {
+        List<Tache> tacheList = tacheRepository.findAll();
+        model.addAttribute("tacheList", tacheList);
+        return "dashboard";
+    }
+    @PostMapping("/save")
+    public void saveTache(Tache tache){
+        tacheRepository.save(tache);
+    }
+    @PostMapping("/update")
+    public void Modif(long id){
+        tacheRepository.getById(id);
+    }
+    @PostMapping("/delete")
+    public void del(long id){
+        tacheRepository.deleteById(id);
     }
 }
