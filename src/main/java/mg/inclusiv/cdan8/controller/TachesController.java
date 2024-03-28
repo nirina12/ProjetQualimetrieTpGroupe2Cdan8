d1903ef87ff9100e5e8ad25e39fc55d2edc8f5bb
gitArnel456 <arnelmihaja@gmail.com>
2024-03-28 09:32:02 +0300

    mis à jour ajout

--- src/main/java/mg/inclusiv/cdan8/controller/TachesController.java
+++ src/main/java/mg/inclusiv/cdan8/controller/TachesController.java
@@ -1,20 +1,22 @@
 package mg.inclusiv.cdan8.controller;
 
 
-import mg.inclusiv.cdan8.Services.TacheService;
-import mg.inclusiv.cdan8.entity.Tache;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
+import mg.inclusiv.cdan8.Services.TacheService;
+import mg.inclusiv.cdan8.entity.Tache;
 
 @Controller
 public class TachesController {
     @Autowired
     TacheService tacheService;
-    @GetMapping("/")
+    
+    @GetMapping("/viewhome")
     public String viewhome(Model model) {
        List<Tache> tacheList= tacheService.tacheList();
         model.addAttribute("tacheList", tacheList);
