ae4168b6d7ea8c6fcd8d7a2eada87680fb3b806b
EliaRandriantsiory <elia.randriantsiory@gmail.com>
2024-04-01 18:09:53 +0300

    commitdasboard

--- src/main/java/mg/inclusiv/cdan8/SpringQualimetriGrp2Application.java
+++ src/main/java/mg/inclusiv/cdan8/SpringQualimetriGrp2Application.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringQualimetriGrp2Application {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(SpringQualimetriGrp2Application.class, args);
 	}
-
 }
